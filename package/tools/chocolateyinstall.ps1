8171f5957ed109d69ff9f4bbba74facecbd64a0e
Trey Morris <treycodes@live.com>
2020-06-11 23:26:19 -0400

    Initial commit.

---
+++ package/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'zunesoftware'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'ZunePackage.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  file         = $fileLocation
+
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
